fb66dd4b31f5a7bd2e6d0a23a15ec4cb665fbd96
Emerson Gaudencio <emerson.fender@gmail.com>
2020-07-22 01:37:15 +0100

    3rd commit

--- automation/install_ansible_mariadb_102.sh
+++ automation/install_ansible_mariadb_102.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 verify_wget=`rpm -qa | grep wget`
-if [[ $verify_wget == "wget"* ]]
-then
+if [ "${verify_wget}" == "wget"* ] ; then
 echo "$verify_wget is installed!"
 else
    sudo yum install wget -y
 fi
 
 verify_python=`rpm -qa | grep python-2.7`
-if [[ $verify_python == "python-2.7"* ]]
-then
+if [ "${verify_python}" == "python-2.7"* ] ; then
 echo "$verify_python is installed!"
 else
    sudo yum install python -y
 fi
 
 verify_git=`rpm -qa | grep git`
-if [[ $verify_git == "git"* ]]
-then
+if [ "${verify_git}" == "git"* ] ; then
 echo "$verify_git is installed!"
 else
    sudo yum install git -y
 fi
 
 verify_pip=`pip -V`
-if [[ $verify_pip == "pip"* ]]
-then
+if [ "${verify_pip}" == "pip"* ] ; then
 echo "$verify_pip is installed!"
 else
    cd /opt
