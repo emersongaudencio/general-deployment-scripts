83c03b9433d53d15f3964512312962d1158a7e08
Emerson Gaudencio <emerson.fender@gmail.com>
2020-08-11 01:52:41 +0100

    config adjustments.

--- automation/install_ansible_mysql_56.sh
+++ automation/install_ansible_mysql_56.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 verify_wget=`rpm -qa | grep wget`
-if [ "${verify_wget}" == "wget"* ] ; then
+if [[ "${verify_wget}" == "wget"* ]] ; then
 echo "$verify_wget is installed!"
 else
    sudo yum install wget -y
 fi
 
 verify_python=`rpm -qa | grep python-2.7`
-if [ "${verify_python}" == "python-2.7"* ] ; then
+if [[ "${verify_python}" == "python-2.7"* ]] ; then
 echo "$verify_python is installed!"
 else
    sudo yum install python -y
 fi
 
-verify_git=`rpm -qa | grep git`
-if [ "${verify_git}" == "git"* ] ; then
+verify_git=`rpm -qa | grep git-1`
+if [[ "${verify_git}" == "git"* ]] ; then
 echo "$verify_git is installed!"
 else
    sudo yum install git -y
 fi
 
 verify_pip=`pip -V`
-if [ "${verify_pip}" == "pip"* ] ; then
+if [[ "${verify_pip}" == "pip"* ]] ; then
 echo "$verify_pip is installed!"
 else
-   cd /opt
-   sudo rm -rf get-pip.py
    sudo wget https://bootstrap.pypa.io/get-pip.py
    sudo python get-pip.py
 fi
 
 ### install ansible #####
-sudo pip install ansible --upgrade
-ansible --version
+verify_ansible=`ansible --version`
+if [[ "${verify_ansible}" == "ansible"* ]] ; then
+echo "$verify_ansible is installed!"
+else
+  sudo pip install ansible --upgrade
+  ansible --version
+fi
 
 cd /opt
 git clone https://github.com/emersongaudencio/ansible-mysql-install-standalone.git
