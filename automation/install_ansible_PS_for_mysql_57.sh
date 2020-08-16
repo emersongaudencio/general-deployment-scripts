1baec6e9a64237a2297a5d718d3dbd2c1ec039d1
Emerson Gaudencio <emerson.fender@gmail.com>
2020-08-16 02:05:02 +0100

    Installation time improvements.

--- automation/install_ansible_PS_for_mysql_57.sh
+++ automation/install_ansible_PS_for_mysql_57.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-verify_wget=`rpm -qa | grep wget`
-if [[ "${verify_wget}" == "wget"* ]] ; then
-echo "$verify_wget is installed!"
-else
-   sudo yum install wget -y
-fi
-
+#### install python2 #####
 verify_python=`rpm -qa | grep python-2.7`
 if [[ "${verify_python}" == "python-2.7"* ]] ; then
 echo "$verify_python is installed!"
@@ -13,6 +7,7 @@ else
    sudo yum install python -y
 fi
 
+#### install git #####
 verify_git=`rpm -qa | grep git-1`
 if [[ "${verify_git}" == "git"* ]] ; then
 echo "$verify_git is installed!"
@@ -20,15 +15,15 @@ else
    sudo yum install git -y
 fi
 
+#### install pip #####
 verify_pip=`pip -V`
 if [[ "${verify_pip}" == "pip"* ]] ; then
 echo "$verify_pip is installed!"
 else
-   sudo wget https://bootstrap.pypa.io/get-pip.py
-   sudo python get-pip.py
+   curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python
 fi
 
-### install ansible #####
+#### install ansible #####
 verify_ansible=`ansible --version`
 if [[ "${verify_ansible}" == "ansible"* ]] ; then
 echo "$verify_ansible is installed!"
