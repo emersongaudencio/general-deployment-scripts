1d844167cf4b9567f6778627a2a81973c18f6175
Emerson Gaudencio <emerson.fender@gmail.com>
2020-09-09 03:00:04 +0100

    adding essential Python packages

--- automation/install_ansible_mysql_57.sh
+++ automation/install_ansible_mysql_57.sh
@@ -2,15 +2,23 @@
 #### install python2 #####
 verify_python=`rpm -qa | grep python-2.7`
 if [[ "${verify_python}" == "python-2.7"* ]] ; then
-echo "$verify_python is installed!"
+   echo "$verify_python is installed!"
+   #### verify python support packages ####
+   verify_PyYAML=`rpm -qa | grep PyYAML`
+   if [[ "${verify_PyYAML}" == "PyYAML"* ]] ; then
+      echo "$verify_PyYAML is installed!"
+   else
+      sudo yum -y install PyYAML libyaml python-babel  python-cffi  python-enum34 python-idna python-jinja2 python-markupsafe python-paramiko  python-ply  python-pycparser python-six python2-cryptography
+   fi
 else
-   sudo yum install python -y
+   sudo yum -y install python
+   sudo yum -y install PyYAML libyaml python-babel  python-cffi  python-enum34 python-idna python-jinja2 python-markupsafe python-paramiko  python-ply  python-pycparser python-six python2-cryptography
 fi
 
 #### install git #####
 verify_git=`rpm -qa | grep git-1`
 if [[ "${verify_git}" == "git"* ]] ; then
-echo "$verify_git is installed!"
+   echo "$verify_git is installed!"
 else
    sudo yum install git -y
 fi
@@ -18,7 +26,7 @@ fi
 #### install pip #####
 verify_pip=`pip -V`
 if [[ "${verify_pip}" == "pip"* ]] ; then
-echo "$verify_pip is installed!"
+   echo "$verify_pip is installed!"
 else
    curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python
 fi
@@ -26,7 +34,7 @@ fi
 #### install ansible #####
 verify_ansible=`ansible --version`
 if [[ "${verify_ansible}" == "ansible"* ]] ; then
-echo "$verify_ansible is installed!"
+  echo "$verify_ansible is installed!"
 else
   sudo pip install ansible --upgrade
   ansible --version
