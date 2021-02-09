c66b34a98234eab1bcfc5fbaf014d7e860ef72f7
Emerson Gaudencio <emerson.fender@gmail.com>
2021-02-09 01:43:44 -0300

    Final update python3 compatibility

--- automation/install_ansible_mysql_80.sh
+++ automation/install_ansible_mysql_80.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
-#### install python2 #####
-verify_python=`rpm -qa | grep python-2.7`
-if [[ "${verify_python}" == "python-2.7"* ]] ; then
+#### install python3 #####
+verify_python=`rpm -qa | grep python-3`
+if [[ "${verify_python}" == "python-3"* ]] ; then
    echo "$verify_python is installed!"
-   #### verify python support packages ####
-   verify_PyYAML=`rpm -qa | grep PyYAML`
-   if [[ "${verify_PyYAML}" == "PyYAML"* ]] ; then
-      echo "$verify_PyYAML is installed!"
-   else
-      sudo yum -y install PyYAML libyaml python-babel  python-cffi  python-enum34 python-idna python-jinja2 python-markupsafe python-paramiko  python-ply  python-pycparser python-six python2-cryptography
-   fi
 else
-   sudo yum -y install python
-   sudo yum -y install PyYAML libyaml python-babel  python-cffi  python-enum34 python-idna python-jinja2 python-markupsafe python-paramiko  python-ply  python-pycparser python-six python2-cryptography
+   yum -y install python3
 fi
 
 #### install git #####
@@ -20,7 +12,7 @@ verify_git=`rpm -qa | grep git-1`
 if [[ "${verify_git}" == "git"* ]] ; then
    echo "$verify_git is installed!"
 else
-   sudo yum install git -y
+   yum install git -y
 fi
 
 #### install pip #####
@@ -28,7 +20,7 @@ verify_pip=`pip -V`
 if [[ "${verify_pip}" == "pip"* ]] ; then
    echo "$verify_pip is installed!"
 else
-   curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python
+   curl -sS https://bootstrap.pypa.io/get-pip.py | python3
 fi
 
 #### install ansible #####
@@ -36,11 +28,12 @@ verify_ansible=`ansible --version`
 if [[ "${verify_ansible}" == "ansible"* ]] ; then
   echo "$verify_ansible is installed!"
 else
-  sudo pip install ansible --upgrade
+  python3 -m pip install ansible
   ansible --version
 fi
 
 cd /opt
 git clone https://github.com/emersongaudencio/ansible-mysql-install-standalone.git
 cd ansible-mysql-install-standalone/ansible
-sudo sh run_mysql_install.sh dblocalhost 80
+sed -ie 's/ansible/\/usr\/local\/bin\/ansible/g' run_mysql_install.sh
+sh run_mysql_install.sh dblocalhost 80
--- automation/sample_user_data.sh
+++ automation/sample_user_data.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# mariadb
 curl -sS https://raw.githubusercontent.com/emersongaudencio/general-deployment-scripts/master/automation/install_ansible_mariadb_104.sh | sudo bash
+
+# mysql example
+curl -sS https://raw.githubusercontent.com/emersongaudencio/general-deployment-scripts/master/automation/install_ansible_mysql_57.sh | sudo bash
