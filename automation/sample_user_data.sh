401e0c6b8cc5e13f9af2a11a22304d4d1f288c3e
Emerson Gaudencio <emerson.fender@gmail.com>
2021-02-10 22:39:51 -0300

    Adjusting ansible deployment

--- automation/sample_user_data.sh
+++ automation/sample_user_data.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # mariadb
-curl -sS https://raw.githubusercontent.com/emersongaudencio/general-deployment-scripts/master/automation/install_ansible_mariadb_104.sh | sudo bash
+curl -sS https://raw.githubusercontent.com/emersongaudencio/general-deployment-scripts/master/automation/install_ansible_mariadb_104.sh | bash
 
 # mysql example
-curl -sS https://raw.githubusercontent.com/emersongaudencio/general-deployment-scripts/master/automation/install_ansible_mysql_57.sh | sudo bash
+curl -sS https://raw.githubusercontent.com/emersongaudencio/general-deployment-scripts/master/automation/install_ansible_mysql_57.sh | bash
+
+# ansible example
+curl -sS https://raw.githubusercontent.com/emersongaudencio/general-deployment-scripts/master/automation/install_ansible_latest.sh | bash
+
+curl -sS https://raw.githubusercontent.com/emersongaudencio/general-deployment-scripts/master/automation/install_ansible_stable-2.9.sh | bash
