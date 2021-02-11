893ae5ebbe122971b82e12cc87ac3a521777971e
Emerson Gaudencio <emerson.fender@gmail.com>
2021-02-10 22:34:02 -0300

    Adjusting Ansible deployment.

--- automation/install_ansible_lastest.sh
+++ automation/install_ansible_lastest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #### install python3 #####
-verify_python=`rpm -qa | grep python-3`
-if [[ "${verify_python}" == "python-3"* ]] ; then
+verify_python=`rpm -qa | grep python3-3`
+if [[ "${verify_python}" == "python3-3"* ]] ; then
    echo "$verify_python is installed!"
 else
    yum -y install python3
@@ -19,15 +19,22 @@ fi
 verify_pip=`pip -V`
 if [[ "${verify_pip}" == "pip"* ]] ; then
    echo "$verify_pip is installed!"
+   echo "Pip-Path: $(which pip)"
 else
    curl -sS https://bootstrap.pypa.io/get-pip.py | python3
+   source ~/.bashrc
+   pip -V
+   echo "Pip-Path: $(which pip)"
 fi
 
 #### install ansible #####
 verify_ansible=`ansible --version`
 if [[ "${verify_ansible}" == "ansible"* ]] ; then
   echo "$verify_ansible is installed!"
+  echo "Ansible-Path: $(which ansible)"
 else
   python3 -m pip install ansible
+  source ~/.bashrc
   ansible --version
+  echo "Ansible-Path: $(which ansible)"
 fi
