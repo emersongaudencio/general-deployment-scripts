b23b0a3df7755f04ef6eb6e95d99338645c78ffe
Emerson Gaudencio <emerson.fender@gmail.com>
2021-07-21 10:47:27 +0100

    added random pwd generator for linux servers

--- automation/random_pwd.sh
+++ automation/random_pwd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #### Generate random password linux systems ####
-random32=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9#$&_+@?' < /dev/urandom | fold -w 32 | head -n 1) && echo $random32
+random32=$(LC_CTYPE=C tr -dc 'a-zA-Z0-9&+@!' < /dev/urandom | fold -w 32 | head -n 1) && echo $random32
