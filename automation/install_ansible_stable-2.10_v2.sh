c83527f77d470d8694b621a968fcd3c06e4d613a
Emerson Gaudencio <emerson.fender@gmail.com>
2021-08-18 02:51:38 +0100

    added ansible support for python3 local install

---
+++ automation/install_ansible_stable-2.10_v2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#### install python3 #####
+verify_python=`rpm -qa | grep python3-3`
+if [[ "${verify_python}" == "python3-3"* ]] ; then
+   echo "$verify_python is installed!"
+else
+   yum -y install python3
+fi
+
+#### install git #####
+verify_git=`rpm -qa | grep git-1`
+if [[ "${verify_git}" == "git"* ]] ; then
+   echo "$verify_git is installed!"
+else
+   yum install git -y
+fi
+
+#### install pip #####
+verify_pip=`pip -V`
+if [[ "${verify_pip}" == "pip"* ]] ; then
+   echo "$verify_pip is installed!"
+   echo "Pip-Path: $(which pip)"
+else
+   curl -sS https://bootstrap.pypa.io/get-pip.py | python3
+   source ~/.bashrc
+   pip -V
+   echo "Pip-Path: $(which pip)"
+fi
+
+#### install ansible #####
+verify_ansible=`ansible --version`
+if [[ "${verify_ansible}" == "ansible"* ]] ; then
+  echo "$verify_ansible is installed!"
+  echo "Ansible-Path: $(which ansible)"
+else
+  cd /opt
+  wget https://github.com/ansible/ansible/archive/stable-2.10.tar.gz
+  tar xzvf stable-2.10.tar.gz
+  python3 -m pip install -e ./ansible-stable-2.10
+  source ~/.bashrc
+  ansible --version
+  echo "Ansible-Path: $(which ansible)"
+fi
