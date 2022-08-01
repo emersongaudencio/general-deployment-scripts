8b0c86a1420bd1474aa6bd4f2734beb9e1531b21
Emerson Gaudencio <emerson.fender@gmail.com>
2022-08-01 01:15:31 +0100

    update awscli-v2

--- automation/install_awscli-v2.sh
+++ automation/install_awscli-v2.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-#### install python3 #####
-verify_python=`rpm -qa | grep python3-3`
-if [[ "${verify_python}" == "python3-3"* ]] ; then
-   echo "$verify_python is installed!"
-else
-   yum -y install python3 unzip
-fi
-
 #### install awscli #####
 verify_awscli=`aws --version`
 if [[ "${verify_awscli}" == "aws"* ]] ; then
   echo "$verify_awscli is installed!"
   echo "AWSCLI-Path: $(which aws)"
 else
+  yum -y install unzip
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
   unzip awscliv2.zip
   sudo ./aws/install
