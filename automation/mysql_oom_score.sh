2510b08fea24669283e1724f0b302fe62a16b57c
Emerson Gaudencio <emerson.fender@gmail.com>
2022-08-26 01:42:24 +0100

    mysql oom score update

--- automation/mysql_oom_score.sh
+++ automation/mysql_oom_score.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
+# set oom score for mysql in linux systems
+echo '#!/bin/bash
 #### set oom score for mysql in linux systems ####
-echo "HOSTNAME: " `hostname`
-echo "[`date +%d/%m/%Y" "%H:%M:%S`]"
-echo "##############"
-MYSQL_PID=$(pgrep mysqld | awk 'NR==1{print $1}')
+MYSQL_PID=$(pgrep mysqld | awk "NR==1{print $1}")
 if test -f /proc/${MYSQL_PID}/oom_score_adj; then
   CURRENT_SCORE=$(cat /proc/${MYSQL_PID}/oom_score_adj)
   if [ "$CURRENT_SCORE" == "0" ]; then
@@ -12,5 +10,6 @@ if test -f /proc/${MYSQL_PID}/oom_score_adj; then
   else
     echo "MySQL OOM Score is already in place!"
   fi
-fi
-echo "##############"
+fi' > /root/mysql_oom_score.sh
+# set cron job to update the mysql oom score every 5 minutes
+(crontab -l; echo "*/5 * * * * root bash /root/mysql_oom_score.sh >/dev/null 2>&1")|awk '!x[$0]++'|crontab -
